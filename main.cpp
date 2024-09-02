19c8c3bb433723b469a637a28bb3348d1a43ff1d
helwan-djibril-mimouni <163547268+helwan-djibril-mimouni@users.noreply.github.com>
2024-09-02 14:02:05 +0200

    moving the tile : done (without gravity)

--- main.cpp
+++ main.cpp
@@ -1,56 +1,134 @@
 #include <SDL2/SDL.h>
-#include <iostream>
 #include <SDL2/SDL_image.h>
-
-const int WIDTH = 1280;
-const int HEIGHT = 739;
-
-int main(int argv, char* args[])
+#include <SDL2/SDL_timer.h>
+#include <vector>
+ 
+int main(int argc, char *argv[])
 {
-    SDL_Surface* windowSurface = nullptr;
-    SDL_Surface* imageSurface = nullptr;
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        std::cout << "couldnt init sdl error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       500, 500, 0);
+ 
+    // triggers the program that controls
+    // your graphics hardware and sets flags
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+ 
+    // creates a renderer to render our images
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+ 
+    // creates a surface to load an image into the main memory
+    SDL_Surface* surface;
+ 
+    // please provide a path for your image
+    surface = IMG_Load("2 Tile.png");
+ 
+    // loads image to our graphics hardware memory.
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+ 
+    // clears main-memory
+    SDL_FreeSurface(surface);
+ 
+    // let us control our image position
+    // so that we can move it with our keyboard.
+    SDL_Rect dest;
 
-    std::string windowTitle = "setup sdl2 with images";
-    SDL_Window* window = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    windowSurface = SDL_GetWindowSurface(window);
-    if (window == nullptr)
-    {
-        std::cout << "window could not be initialized: ERROR: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    if(!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
-    {
-        std::cout << "could not initialize sdl image jpeg Error: " << IMG_GetError() << std::endl;
-        return 1;
-    }
-    
-    SDL_Event windowEvent;
-
-    imageSurface = IMG_Load("fin and jake.jpeg");
-
-    std::cout << "image width " << imageSurface->w << " image height " << imageSurface->h;
-
-    while(true)
-    {
-        if(SDL_PollEvent(&windowEvent))
-        {
-            if(windowEvent.type == SDL_QUIT)
-            {
+    // List of Rect to move all rects at once later
+    std::vector<SDL_Rect> rects;
+    rects.push_back(dest);
+ 
+    // connects our texture with dest to control position
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+ 
+    // sets initial x-position of object
+    dest.x = 0;
+ 
+    // sets initial y-position of object
+    dest.y = 0;
+ 
+    // controls animation loop
+    int close = 0;
+ 
+    // speed of box
+    int speed = 300;
+ 
+    // animation loop
+    while (!close) {
+        SDL_Event event;
+ 
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+ 
+            case SDL_QUIT:
+                // handling of close button
+                close = 1;
                 break;
+ 
+            case SDL_KEYDOWN:
+                // keyboard API for key pressed
+                switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    dest.y -= 125;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    dest.x -= 125;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    dest.y += 125;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    dest.x += 125;
+                    break;
+                default:
+                    break;
+                }
             }
         }
-        SDL_BlitSurface(imageSurface, nullptr, windowSurface, nullptr);
-        SDL_UpdateWindowSurface(window);
-
+ 
+        // right boundary
+        if (dest.x + dest.w > 500)
+            dest.x = 500 - dest.w;
+ 
+        // left boundary
+        if (dest.x < 0)
+            dest.x = 0;
+ 
+        // bottom boundary
+        if (dest.y + dest.h > 500)
+            dest.y = 500 - dest.h;
+ 
+        // upper boundary
+        if (dest.y < 0)
+            dest.y = 0;
+ 
+        // clears the screen
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &dest);
+ 
+        // triggers the double buffers
+        // for multiple rendering
+        SDL_RenderPresent(rend);
+ 
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
     }
-    
-    SDL_DestroyWindow(window);
-
+ 
+    // destroy texture
+    SDL_DestroyTexture(tex);
+ 
+    // destroy renderer
+    SDL_DestroyRenderer(rend);
+ 
+    // destroy window
+    SDL_DestroyWindow(win);
+     
+    // close SDL
+    SDL_Quit();
+ 
     return 0;
 }
