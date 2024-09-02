25f06ccf6c50242b5d77f5bd5f97b6a9d1b0bc87
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-02 15:06:12 +0200

    hpp & cpp file manage work

--- main.cpp
+++ main.cpp
@@ -1,56 +1,8 @@
-#include <SDL2/SDL.h>
+#include "Window.hpp"
 #include <iostream>
-#include <SDL2/SDL_image.h>
 
-const int WIDTH = 1280;
-const int HEIGHT = 739;
-
-int main(int argv, char* args[])
-{
-    SDL_Surface* windowSurface = nullptr;
-    SDL_Surface* imageSurface = nullptr;
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        std::cout << "couldnt init sdl error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    std::string windowTitle = "setup sdl2 with images";
-    SDL_Window* window = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    windowSurface = SDL_GetWindowSurface(window);
-    if (window == nullptr)
-    {
-        std::cout << "window could not be initialized: ERROR: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    if(!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
-    {
-        std::cout << "could not initialize sdl image jpeg Error: " << IMG_GetError() << std::endl;
-        return 1;
-    }
-    
-    SDL_Event windowEvent;
-
-    imageSurface = IMG_Load("fin and jake.jpeg");
-
-    std::cout << "image width " << imageSurface->w << " image height " << imageSurface->h;
-
-    while(true)
-    {
-        if(SDL_PollEvent(&windowEvent))
-        {
-            if(windowEvent.type == SDL_QUIT)
-            {
-                break;
-            }
-        }
-        SDL_BlitSurface(imageSurface, nullptr, windowSurface, nullptr);
-        SDL_UpdateWindowSurface(window);
-
-    }
-    
-    SDL_DestroyWindow(window);
+int main(int argc, char* argv[]) {
+    Window window;
 
     return 0;
 }
