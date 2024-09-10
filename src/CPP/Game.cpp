b33dbd5c9ca5cc3927cb91a1149c10310552cfed
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-10 10:08:52 +0200

    Create class Game to manage display of tile

---
+++ src/CPP/Game.cpp
@@ -0,0 +1,31 @@
+#include "../HPP/Game.hpp"
+#include "../HPP/Tile.hpp"
+
+
+Game::Game(){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            if (matrix.matrix[i][j] != 0){
+                Tile tile = Tile(168 + (125*j), 120 + (125*i) , 125, 125, matrix.matrix[i][j]);
+                tiles.push_back(tile);
+            }   
+        }
+    }
+}
+
+Game::~Game(){
+    // Destructor
+}
+
+void Game::update(){
+    tiles.clear();
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            if (matrix.matrix[i][j] != 0){
+                Tile tile = Tile(168 + (125*j), 120 + (125*i) , 125, 125, matrix.matrix[i][j]);
+                tiles.push_back(tile);
+            }   
+        }
+    }
+}
+
---
+++ src/HPP/Game.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "Matrix.hpp"
+#include "Tile.hpp"
+#include <vector>
+
+class Game : public Matrix{
+    
+    public:
+        Matrix matrix = Matrix();
+        std::vector<Tile> tiles;
+        Game();
+        ~Game();
+        void update();
+
+};
+
+#endif
