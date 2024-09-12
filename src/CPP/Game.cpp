8d4b8e60d6b89483aa27cbc2019520c71a456df7
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-09-12 08:49:46 +0200

    move logic to Create GameObject Tile and add this in a list to a specific Method

--- src/CPP/Game.cpp
+++ src/CPP/Game.cpp
@@ -3,14 +3,7 @@
 
 
 Game::Game(){
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            if (matrix.matrix[i][j] != 0){
-                Tile tile = Tile(168 + (125*j), 120 + (125*i) , 125, 125, matrix.matrix[i][j]);
-                tiles.push_back(tile);
-            }   
-        }
-    }
+    this->createTile();
 }
 
 Game::~Game(){
@@ -19,6 +12,10 @@ Game::~Game(){
 
 void Game::update(){
     tiles.clear();
+    this->createTile();
+}
+
+void Game::createTile(){
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++){
             if (matrix.matrix[i][j] != 0){
@@ -28,4 +25,3 @@ void Game::update(){
         }
     }
 }
-
--- src/HPP/Game.hpp
+++ src/HPP/Game.hpp
@@ -13,6 +13,7 @@ class Game : public Matrix{
         Game();
         ~Game();
         void update();
+        void createTile();
 
 };
 
